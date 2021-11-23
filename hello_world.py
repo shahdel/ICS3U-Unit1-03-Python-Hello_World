92fe52cb74c38475543f046164993410fb2d9a8d
EC2 Default User <ec2-user@ip-172-31-45-129.ec2.internal>
2021-11-23 17:41:50 +0000

    added hello world

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+#!/usr/bin/env python3 
+
+# Created by Shahd Elsaman
+# Created on November 2021
+# This is the "Hello, World!" program, with proper style
+
+
+def main():
+    print("Hello, World!")
+    
+    
+if __name__== "__main__":
+    main()
